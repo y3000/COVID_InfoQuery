d21ff0d93c1612a625c887f25b23f577beae58be
董泽平 <dzp19903@163.com>
2022-10-03 20:37:08 +0800

    feat: 首页引导完成

--- COVID_InfoQuery/components/policyQuery/policyQuery.vue
+++ COVID_InfoQuery/components/policyQuery/policyQuery.vue
@@ -1,6 +1,8 @@
 <template>
 	<view class="title" @click="toPolicy">
-		疫情出行政策
+		
+		<view>疫情出行政策</view>
+		<button >一键查询出行政策</button>
 	</view>
 </template>
 
@@ -19,6 +21,20 @@
 
 <style scoped>
 	.title{
-		
+		width: 100vw;
+		padding: 40rpx;
+		box-sizing: border-box;
+		color: #555;
+		font-weight: bold;
+	}
+	button{
+		background-color: #07c160;
+		border:1rpx solid #07c160;
+		color:#fff;
+		text-align: center;
+		border-radius: 50rpx;
+		margin: 40rpx 0 30rpx 0;
+		padding: 10rpx 0;
+		font-weight: normal;
 	}
 </style>
