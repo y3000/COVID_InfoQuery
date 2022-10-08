bb83c7027d8155695a32e620b676e761e1e7e3d6
importance <2428896862@qq.com>
2022-10-08 09:36:22 +0800

    aiot myV1

---
+++ COVID_InfoQuery/pages/aiOt/aiOt.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		智能物联网AIOT
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
