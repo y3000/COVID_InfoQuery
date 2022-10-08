be8c180ce060fa23bcc924ca000a017e626d108f
董泽平 <dzp19903@163.com>
2022-10-08 18:02:04 +0800

    fix: 页面修复

--- COVID_InfoQuery/pages/newsInfo/newsInfo.vue
+++ COVID_InfoQuery/pages/newsInfo/newsInfo.vue
@@ -1,22 +1,22 @@
 <template>
-	<web-view src="https://content-static.cctvnews.cctv.com/snow-book/index.html?item_id=18389426101373366790&toc_style_id=feeds_default"></web-view>
+  <web-view :src="url"></web-view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				url: "",
-			}
-		},
-		onLoad(e){
-			console.log(e);
-			this.url =decodeURIComponent(e.sourceUrl);
-		},
-		methods: {
-			
-		}
-	}
+  export default {
+    data() {
+      return {
+        url: "",
+      }
+    },
+    onLoad(e){
+      console.log(e);
+      this.url +=JSON.parse( decodeURIComponent(e.sourceUrl));
+    },
+    methods: {
+      
+    }
+  }
 </script>
 
 <style>
